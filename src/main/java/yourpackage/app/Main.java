910b624003a606ddb76ed511f68567d7db555536
Hayden <hve0002@uah.edu>
2023-10-19 09:32:44 -0500

    updates video navbar

--- src/main/java/yourpackage/app/Main.java
+++ src/main/java/yourpackage/app/Main.java
@@ -1,44 +1,8 @@
-package yourpackage.visualization;
+package yourpackage.app;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.Scanner;
-
-
-/* THIS IS A TEST FILE TO TEST THE VIDEO PLAYING COMPONENTS OF THE PROGRAM
- * THIS DOES NOT RUN ON NORMAL PROGRAM USAGE
- */
-
-public class VideoTester {
-    public static void run(String[] args) {
-        // Create a new JFrame
-        JFrame frame = new JFrame("Swing Video Player");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-
-        // Add a label to the JFrame
-        JLabel label = new JLabel("Welcome to Video Player!");
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        label.setFont(new Font("Arial", Font.BOLD, 24));
-        frame.add(label, BorderLayout.NORTH);
-
-        // Set the JFrame to be visible
-        frame.setVisible(true);
-
-        // Get video file path from user input using Scanner
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the path to the video file: ");
-        String videoFilePath = scanner.nextLine();
-
-
-        // Check if the entered file path is not empty
-        if (!videoFilePath.isEmpty()) {
-            VideoPlayerSwingIntegration.embedVideoIntoJFrame(frame, videoFilePath);
-        } else {
-            System.out.println("Invalid video file path. Please provide a valid path.");
-        }
-
-        // Close the scanner
-        scanner.close();
-    }
+import com.formdev.flatlaf.FlatDarkLaf;
+public class Main {
+    public static void main (String[] args)
+    { FlatDarkLaf.setup(); // Initialize Flatlaf, the look and feel we are using for Swing.
+        Window window = new Window(); }
 }
--- src/main/java/yourpackage/visualization/VideoPlayerSwingIntegration.java
+++ src/main/java/yourpackage/visualization/VideoPlayerSwingIntegration.java
@@ -7,11 +7,15 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.util.Duration;
 
 import javax.swing.*;
+import javax.swing.event.ChangeListener;
 import java.io.File;
 
 public class VideoPlayerSwingIntegration {
+
+    private static MediaPlayer player;
 
     public static void embedVideoIntoJFrame(JFrame frame, String videoFilePath) {
         File videoFile = new File(videoFilePath);
@@ -26,7 +30,7 @@ public class VideoPlayerSwingIntegration {
 
         Platform.runLater(() -> {
             Media media = new Media(videoFile.toURI().toString());
-            MediaPlayer player = new MediaPlayer(media);
+            player = new MediaPlayer(media);
             MediaView viewer = new MediaView(player);
 
             viewer.setPreserveRatio(true);
@@ -57,5 +61,67 @@ public class VideoPlayerSwingIntegration {
 
             frame.setVisible(true);
         });
+    }
+
+    public void play() {
+        if (player != null) {
+            player.play();
+        }
+    }
+
+    public void pause() {
+        if (player != null) {
+            player.pause();
+        }
+    }
+
+    public void stop() {
+        System.out.println("stop!");
+        if (player != null) {
+            player.stop();
+        }
+    }
+
+    public void forward() {
+        if (player != null) {
+            Duration currentTime = player.getCurrentTime();
+            player.seek(currentTime.add(Duration.seconds(10))); // Forward by 10 seconds
+        }
+    }
+
+    public void rewind() {
+        if (player != null) {
+            Duration currentTime = player.getCurrentTime();
+            player.seek(currentTime.subtract(Duration.seconds(10))); // Rewind by 10 seconds
+        }
+    }
+
+    public double getCurrentTimeInSeconds() {
+        if (player != null) {
+            Duration currentTime = player.getCurrentTime();
+            return currentTime.toSeconds();
+        }
+        return 0.0;
+    }
+
+    public double getTotalDurationInSeconds() {
+        if (player != null) {
+            Duration totalDuration = player.getTotalDuration();
+            return totalDuration.toSeconds();
+        }
+        return 0.0;
+    }
+
+    public void skipToTime(double timeInSeconds) {
+        if (player != null) {
+            player.seek(Duration.seconds(timeInSeconds));
+        }
+    }
+
+    // Method to add a time listener to update the slider position
+    public void addTimeListener(ChangeListener listener) {
+        if (player != null) {
+            player.currentTimeProperty().addListener((javafx.beans.value.ChangeListener<? super Duration>) listener);
+        }
     }
 }
