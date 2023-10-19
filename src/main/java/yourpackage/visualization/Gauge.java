0d404cf02d2151ff90004a722f5d2e83607df396
Satero Vazquez <98067321+sav0007@users.noreply.github.com>
2023-10-19 10:06:38 -0500

    Update Gauge.java
    added some fields and the functions to be worked on later. Also gave the enum GaugeType so we can differentiate.

---
+++ src/main/java/yourpackage/visualization/Gauge.java
@@ -0,0 +1,55 @@
+package yourpackage.gauges;
+
+import javax.swing.*;
+import java.awt.*;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import eu.hansolo.steelseries.gauges.Radial;
+
+
+public class Gauge {
+    private String name;
+    private float size;
+    private float xpos;
+    private float ypos;
+    //private Datafield correspondingField;
+    private float blueLimit;
+    private float greenLimit;
+    private float yellowLimit;
+    private float redLimit;
+
+    public Gauge()
+    {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setMinimumSize(new Dimension(250, 250));
+        frame.setTitle("gaugeName");
+        frame.setVisible(true);
+    }
+    public enum GaugeType {
+        Circle,
+        Bar,
+        XPlot,
+        XByYPLOT,
+        NumOrSingleChar,
+        xtDisplay,
+        Stopwatch,
+        OnOffLight,
+    }
+    public void resize(){
+
+    }
+    public void playAlarm(){
+
+    }
+}
