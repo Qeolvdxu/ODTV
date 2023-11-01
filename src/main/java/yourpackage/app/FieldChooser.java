44e09dafdae9b8a8c13944c6197b97db5b781c0c
SleepySterling <142691648+SleepySterling@users.noreply.github.com>
2023-10-31 21:08:41 -0500

    Selection and removal of fields
    Fields can now be selected from the GUI and will be stored in an ArrayList. They can also be removed from the selectedFields ArrayList. I still need to implement searching for the foundFields.

--- src/main/java/yourpackage/app/FieldChooser.java
+++ src/main/java/yourpackage/app/FieldChooser.java
@@ -15,18 +15,20 @@ public class FieldChooser {
     private JPanel mainPanelFC;
     private JButton addButton;
     private JButton removeButton;
-    private JList list2;
+    private JList selectedFieldsJList;
     private JScrollPane foundFieldsScrollPane;
     private JList foundFieldsJList;
-    private JButton button1;
+    private JTextField searchTextField;
+    private JButton searchButton;
     private static FieldChooser instance;
     private final JFrame frame;
     private static ArrayList<DataField> foundFields; // Fields found by the parser
     private ArrayList<DataField> selectedFields; // Fields selected by the user
 
-    FieldChooser() {
+    public FieldChooser() {
         frame = new JFrame();
         foundFields = new ArrayList<>();
+        selectedFields = new ArrayList<>();
         String iconPath = System.getProperty("user.dir") + "/resources/icon.png";
         ImageIcon img = new ImageIcon(iconPath);
         frame.setIconImage(img.getImage()); // Get and set a custom icon for the GUI.
@@ -35,26 +37,74 @@ public class FieldChooser {
         frame.pack();
         frame.setResizable(false);
         frame.setVisible(true);
-    }
-
-    public void remove() // The FieldChooser instance will be removed when a new .csv file is opened
-    {
-        instance = null;
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                int selectedFieldIndex = foundFieldsJList.getSelectedIndex();
+                DataField selectedField;
+                selectedField = foundFields.get(selectedFieldIndex);
+                if (selectedFields.size() < 10 && !fieldExists(selectedField)) {
+                    addSelectedField(selectedField);
+                } else {
+                    System.out.println("Could not add field!");
+                }
+            }
+        });
+        removeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                int selectedFieldIndex = selectedFieldsJList.getSelectedIndex();
+                DataField selectedField;
+                System.out.println(selectedFieldIndex);
+                selectedField = selectedFields.get(selectedFieldIndex);
+                removeSelectedField(selectedFieldIndex);
+            }
+        });
     }
 
     public void setFoundFields(ArrayList<DataField> fields) {
         foundFields.addAll(fields);
         System.out.println(foundFields);
-        populateJList();
+        populateFoundFieldsJList();
     }
 
-    private void populateJList()
-    {
+    private boolean fieldExists(DataField field) {
+        boolean exists = false;
+        for (DataField i : selectedFields) {
+            if (i.getFieldName() == field.getFieldName()) {
+                System.out.println("iT lOoKS liEk dA fIeLD Ex1Sts!");
+                exists = true;
+            }
+        }
+        return exists;
+    }
+
+    private void addSelectedField(DataField field) {
+        selectedFields.add(field);
+        System.out.println(selectedFields);
+        populateSelectedFieldsJList();
+    }
+
+    private void removeSelectedField(int index) {
+        selectedFields.remove(index);
+        System.out.println(selectedFields);
+        populateSelectedFieldsJList();
+    }
+
+    private void populateFoundFieldsJList() {
         DefaultListModel<String> listModel = new DefaultListModel<>();
         for (DataField item : foundFields) {
             listModel.addElement(String.valueOf(item));
         }
         foundFieldsJList.setModel(listModel);
+    }
+
+    private void populateSelectedFieldsJList() {
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        for (DataField item : selectedFields) {
+            listModel.addElement(String.valueOf(item));
+        }
+        selectedFieldsJList.setModel(listModel);
     }
 
     {
@@ -73,31 +123,37 @@ public class FieldChooser {
      */
     private void $$$setupUI$$$() {
         mainPanelFC = new JPanel();
-        mainPanelFC.setLayout(new GridLayoutManager(6, 4, new Insets(0, 0, 0, 0), -1, -1));
+        mainPanelFC.setLayout(new GridLayoutManager(7, 4, new Insets(0, 0, 0, 0), -1, -1));
         final Spacer spacer1 = new Spacer();
-        mainPanelFC.add(spacer1, new GridConstraints(1, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        mainPanelFC.add(spacer1, new GridConstraints(2, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final Spacer spacer2 = new Spacer();
-        mainPanelFC.add(spacer2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
-        final JLabel label1 = new JLabel();
-        label1.setText("Add Fields");
-        mainPanelFC.add(label1, new GridConstraints(0, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        mainPanelFC.add(spacer2, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         addButton = new JButton();
         addButton.setText("Add");
-        mainPanelFC.add(addButton, new GridConstraints(2, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        final JLabel label2 = new JLabel();
-        label2.setText("Selected Fields");
-        mainPanelFC.add(label2, new GridConstraints(3, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        mainPanelFC.add(addButton, new GridConstraints(3, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label1 = new JLabel();
+        label1.setText("Selected Fields");
+        mainPanelFC.add(label1, new GridConstraints(4, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         removeButton = new JButton();
         removeButton.setText("Remove");
-        mainPanelFC.add(removeButton, new GridConstraints(5, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        mainPanelFC.add(removeButton, new GridConstraints(6, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         foundFieldsScrollPane = new JScrollPane();
-        mainPanelFC.add(foundFieldsScrollPane, new GridConstraints(1, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        mainPanelFC.add(foundFieldsScrollPane, new GridConstraints(2, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         foundFieldsJList = new JList();
         foundFieldsScrollPane.setViewportView(foundFieldsJList);
         final JScrollPane scrollPane1 = new JScrollPane();
-        mainPanelFC.add(scrollPane1, new GridConstraints(4, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
-        list2 = new JList();
-        scrollPane1.setViewportView(list2);
+        mainPanelFC.add(scrollPane1, new GridConstraints(5, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        selectedFieldsJList = new JList();
+        scrollPane1.setViewportView(selectedFieldsJList);
+        searchTextField = new JTextField();
+        searchTextField.setText("");
+        mainPanelFC.add(searchTextField, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        searchButton = new JButton();
+        searchButton.setText("Search");
+        mainPanelFC.add(searchButton, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label2 = new JLabel();
+        label2.setText("Add Fields");
+        mainPanelFC.add(label2, new GridConstraints(1, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     }
 
     /**
